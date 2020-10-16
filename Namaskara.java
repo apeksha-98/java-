3f2a122e1d28bf54a9e2401d3db829db49d3e472
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-16 20:19:38 +0530

    first  commit

---
+++ Namaskara.java
@@ -0,0 +1,15 @@
+class Wish
+{
+	public static void main(String[] param)
+	{
+		System.out.println("Namaskara");
+	}
+}
+class Camera
+{
+	public static void main(String[] param)
+	{
+		System.out.println("camera is kodak");
+		System.out.print("price is lot");
+	}
+}
