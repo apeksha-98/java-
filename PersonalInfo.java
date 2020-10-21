257a354e7a4165337c9fd589b7b47ff69be689bf
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-22 00:53:40 +0530

    Add files via upload

---
+++ PersonalInfo.java
@@ -0,0 +1,12 @@
+public class PersonalInfo
+{
+	public static void main(String[] params)
+	{
+		final long aadharNumber=654734228973L;
+		System.out.println("aadharNumber:"+aadharNumber);
+		final long phoneNumber=8976548321L;
+		System.out.println("phoneNumber:"+phoneNumber);
+		final String email="apeksha.xworkz@gmail.com";
+		System.out.println("email:"+email);
+	}
+}
