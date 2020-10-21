83d8efd8a061a9fc6800f3b86073a4d09d35df4b
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-22 00:46:34 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,16 @@
+public class Information
+{
+	public static void main(String[] params)
+	{
+		printEmail("apeksha.xworkz@gmail.com");
+		printProgrammingLanguage("java");
+	}
+	public static void printEmail(String email)
+	{
+		System.out.println("email:"+email);
+	}
+	public static void printProgrammingLanguage(String programmingLanguage)
+	{
+		System.out.println("programmingLanguage:"+programmingLanguage);
+	}
+} 
