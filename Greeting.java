93225f6459b812ae47ca7bfb23500505d5df93dd
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-22 00:50:10 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting
+{
+	public static void main(String[] params)
+	{
+		printWelcome("welcome");
+	}
+	public static void printWelcome(String greet)
+	{
+		System.out.println("greet:"+greet);
+	}
+} 
