cc9b40d67a469fd3414fb3981ba2cf9b879b2d44
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-22 23:15:15 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,25 @@
+public class States
+{
+	public static void main(String[] name)
+	{
+		System.out.println("state name " +name);
+		int size=name.length;
+		System.out.println("length of elements " +size);
+		System.out.println("element at 0" +name[0]);
+		System.out.println("element at 1" +name[1]);
+		System.out.println("element at 2" +name[2]);
+		System.out.println("element at 3" +name[3]);
+		System.out.println("element at 4" +name[4]);
+		System.out.println("element at 5" +name[5]);
+		System.out.println("element at 6" +name[6]);
+		System.out.println("element at 7" +name[7]);
+		System.out.println("element at 8" +name[8]);
+		System.out.println("element at 9" +name[9]);
+		System.out.println("element at 10" +name[10]);
+		System.out.println("element at 11" +name[11]);
+		System.out.println("element at 12" +name[12]);
+		System.out.println("element at 13" +name[13]);
+		System.out.println("element at 14" +name[14]);
+	}
+	
+}
