6e4920b847255df419187a631afcd958916a87bb
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-22 23:15:50 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table
+{
+	public static void main(String[] trainees)
+	{
+		System.out.println("trainees " +trainees);
+		int sizeOfMainParam=trainees.length;
+		System.out.println("sizeOfMainParam " +sizeOfMainParam);
+		System.out.println("element at 0" +trainees[0]);
+		System.out.println("element at 1" +trainees[1]);
+	}
+}
