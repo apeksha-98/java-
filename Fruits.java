2decadd53c7b99d9d6088104fa33ab4050dea63e
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:55:47 +0530

    Add files via upload

---
+++ Fruits.java
@@ -0,0 +1,27 @@
+class Fruits
+{
+	public static void main(String[] params)
+	{
+		System.out.println("types of fruits");
+		m1();
+	}
+	public static void m1()
+	{
+		System.out.println("apple");
+		m2();
+	}
+	public static void m2()
+	{
+		System.out.println("mango");
+		m3();
+	}
+	public static void m3()
+	{
+		System.out.println("grapes");
+		m4();
+	}
+	public static void m4()
+	{
+		System.out.println("banana");
+	}
+}
