b21a8ff31e18ccb765e2c1ddbb63ce97bf6d6084
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:36:59 +0530

    Add files via upload

---
+++ Fan.java
@@ -0,0 +1,10 @@
+public class Fan
+{
+public static void main(String[] nameOfPlayers)
+{
+System.out.println("JVM invoked main");
+
+Sports.cricket(nameOfPlayers);
+System.out.println("exit main");	
+}
+}
