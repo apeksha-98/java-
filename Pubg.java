4aaf4a11fdc1312fd46b0b2b3372d0aae1dbf69a
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:36:05 +0530

    Add files via upload

---
+++ Pubg.java
@@ -0,0 +1,12 @@
+public class Pubg
+{
+	public static void supply()
+	{
+		System.out.println("invoked supply from pubg class");
+	}
+	public static void enemiesAhead()
+	{
+		System.out.println("invoked enemiesAhead from pubg class");
+	}
+	
+}
