653045e29d71e59f10731ed6fed5298c6d60aa16
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:55:01 +0530

    Add files via upload

---
+++ Cars.java
@@ -0,0 +1,14 @@
+class Cars
+{
+	public static void main(String[] params)
+	{
+		System.out.println("creta");
+		write();
+		write();
+		write();
+	}
+	public static void write()
+	{
+		System.out.println("BMW");
+	}
+}
