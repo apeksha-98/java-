486f2d1b9f75c8cc663e61bc8f9db9337effde44
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:45:00 +0530

    Add files via upload

---
+++ Datatype3.java
@@ -0,0 +1,33 @@
+public class Datatype3
+{
+	public static void main(String[] params)
+	{
+		byte continents;
+		continents=7;
+		System.out.println(continents);
+		short costOfwatchs;
+		costOfwatchs=3000;
+		System.out.println(costOfwatchs);
+		int distanceOfEarthToMoon;
+		distanceOfEarthToMoon=384400;
+		System.out.println(distanceOfEarthToMoon);
+		long numberOfSpecies;
+		numberOfSpecies=870000000;
+		System.out.println(numberOfSpecies);
+		float height;
+		height=5.3f;
+		System.out.println(height);
+		double valueOfPi;
+		valueOfPi=3.141592653589;
+		System.out.println(valueOfPi);
+		boolean oneIsOdd;
+		oneIsOdd=true;
+		System.out.println(oneIsOdd);
+		char bloodGroup;
+		bloodGroup='B';
+		System.out.println(bloodGroup);
+		String week;
+		week="tuesday";
+		System.out.println(week);
+	}
+} 
