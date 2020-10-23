975ca182e054e4a96584adb5d3f528cc7edf5747
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:45:51 +0530

    Add files via upload

---
+++ Datatype4.java
@@ -0,0 +1,33 @@
+public class Datatype4
+{
+	public static void main(String[] params)
+	{
+		byte numberOfStatesInIndia;
+		numberOfStatesInIndia=29;
+		System.out.println(numberOfStatesInIndia);
+		short monthlyRentOfHome;
+		monthlyRentOfHome=10000;
+		System.out.println(monthlyRentOfHome);
+		int costOfIphone11;
+		costOfIphone11=60000;
+		System.out.println(costOfIphone11);
+		long businessInvestment;
+		businessInvestment=700000000;
+		System.out.println(businessInvestment);
+		float refractiveIndex;
+	    refractiveIndex=1.33f;
+		System.out.println(refractiveIndex);
+		double avogardoNumber;
+		avogardoNumber=6.0221407621;
+		System.out.println(avogardoNumber);
+		boolean zeroIsLessThanOne;
+		zeroIsLessThanOne=true;
+		System.out.println(zeroIsLessThanOne);
+		char result ;
+		result='p';
+		System.out.println(result);
+		String month;
+		month="october";
+		System.out.println(month);
+	}
+} 
