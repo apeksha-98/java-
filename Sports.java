24bdde952c800109f497e7b001fc7e358bea930f
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:37:47 +0530

    Add files via upload

---
+++ Sports.java
@@ -0,0 +1,8 @@
+public class Sports
+{
+	public static void cricket(String[] nameOfPlayer)
+	{
+		System.out.println("player 0 " +nameOfPlayer[0]);
+		System.out.println("player 1 " +nameOfPlayer[1]);
+	}
+}
