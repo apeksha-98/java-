f7de888a7f2558916dbc543b885939e553ae84fd
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:33:46 +0530

    Add files via upload

---
+++ Toy.java
@@ -0,0 +1,10 @@
+public class Toy
+{
+	public static  void main(String[] toys)
+	{
+		System.out.println("JVM invoked main");
+		Pubg.supply();
+		Pubg.enemiesAhead();
+		System.out.println("exit main");
+	}
+}
