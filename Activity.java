f073204a7f825bc208b7165e9271605695cc9b3d
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:53:23 +0530

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,14 @@
+class Activity
+{
+public static void main(String[] params){
+	System.out.println("I m acting");
+	music();
+}
+public static void music(){
+	System.out.println("playing music");
+	drinking();
+}
+public static void drinking(){
+	System.out.println("drinking water");
+}	
+}
