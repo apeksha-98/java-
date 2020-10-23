1e2a97789e64c682bddf9bd74c1498f01e7982d6
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:43:22 +0530

    Add files via upload

---
+++ Datatype2.java
@@ -0,0 +1,33 @@
+public class Datatype2
+{
+	public static void main(String[] params)
+	{
+		byte daysInWeek;
+		daysInWeek=7;
+		System.out.println(daysInWeek);
+		short year;
+		year=2020;
+		System.out.println(year);
+		int speedOfLight;
+		speedOfLight=299792458;
+		System.out.println(speedOfLight);
+		long worldPopulation;
+		worldPopulation=780000000;
+		System.out.println(worldPopulation);
+		float cgpa;
+		cgpa=7.53f;
+		System.out.println(cgpa);
+		double planksConstant;
+		planksConstant=6.62607004;
+		System.out.println(planksConstant);
+		boolean carHasTwoWheels;
+		carHasTwoWheels=false;
+		System.out.println(carHasTwoWheels);
+		char divisionOfClass;
+		divisionOfClass='B';
+		System.out.println(divisionOfClass);
+		String nameOfState;
+		nameOfState="Karnataka";
+		System.out.println(nameOfState);
+	}
+} 
