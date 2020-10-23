bbde3efc2e39e1166a671ae63266da15756bd1dc
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-10-23 22:40:58 +0530

    Add files via upload

---
+++ Datatype1.java
@@ -0,0 +1,34 @@
+public class Datatype1
+{
+	public static void main(String[] params)
+	{
+		byte age;
+		age=23;
+		System.out.println(age);
+		short daysInYear;
+		daysInYear=365;
+		System.out.println(daysInYear);
+		int numberOfEngineersInIndia;
+		numberOfEngineersInIndia=15000000;
+		System.out.println(numberOfEngineersInIndia);
+		long populationOfIndia;
+		populationOfIndia=1380004385;
+		System.out.println(populationOfIndia);
+		float percentageOfStudent;
+		percentageOfStudent=77.5f;
+		System.out.println(percentageOfStudent);
+		double chargeOfElectron;
+		chargeOfElectron=1.602176662;
+		System.out.println(chargeOfElectron);
+		boolean earthIsFlat;
+		earthIsFlat=false;
+		System.out.println(earthIsFlat);
+		char grade;
+		grade='B';
+		System.out.println(grade);
+		String name;
+		name="apeksha";
+		System.out.println(name);
+		
+	}
+}
