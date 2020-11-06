dc3af2f724ba0db5653956016efd1aacb651492e
apeksha-98 <72956345+apeksha-98@users.noreply.github.com>
2020-11-06 23:58:27 +0530

    Add files via upload

---
+++ encapsulation/Reliance.java
@@ -0,0 +1,12 @@
+public class Reliance{
+	
+	private static double budget=45.67;
+	
+	//getter 
+	public static double getBudget()
+	{
+		System.out.println("budget is read only");
+		return budget;
+	}
+	
+}
